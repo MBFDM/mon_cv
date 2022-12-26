f309fabe4cd4465a3114eb000d017629aa06ff63
MBFDM <119826728+MBFDM@users.noreply.github.com>
2022-12-26 16:33:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    $("html").niceScroll({
+
+        cursorcolor: "#212b37",
+        cursorborder: "5px solid #212b37",
+        cursorborderradius: "0",
+        scrollspeed: 65
+    })
+
+    $("ul, div").filter("#scrolls").niceScroll({
+
+        cursorcolor: "#2E060B",
+        cursorborder: "2px solid #981827",
+        cursorborderradius: "0",
+        scrollspeed: 65
+    })
+
+
+});
